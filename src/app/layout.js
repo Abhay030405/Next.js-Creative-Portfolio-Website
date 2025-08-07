78f4bd665d16a4b034dcb995a39aebe289f6e9a8
Abhay Agarwal <officialabhay030405@gmail.com>
2025-08-07 17:48:34 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,12 +12,12 @@ const inter = Inter({
 export const metadata = {
   title: {
     template:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS | %s | CodeBucks",
+      "Abhay Agarwal — AI/ML Enthusiast & Data Science Explorer",
     default:
-      "Next.js Portfolio Created with Three.js and Tailwind CSS by CodeBucks",
+      "Abhay Agarwal — AI/ML Enthusiast & Data Science Explorer",
   },
   description:
-    "A unique creative portfolio designed by CodeBucks with cutting-edge technologies like Next.js, Tailwind CSS, Three.js, and Framer Motion. Experience the art of modern web development firsthand. Checkout CodeBucks on youtube.",
+    "I'm an aspiring Machine Learning Engineer passionate about solving real-world problems through data, intelligent systems, and efficient code.",
 };
 
 export default function RootLayout({ children }) {
